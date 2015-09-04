4e0b6542d50cc5338a1eef90845b5abd64d78776
Christian Busse <christian.busse@dkfz-heidelberg.de>
2015-09-04 12:13:12 +0200

    Fix regular expression to parse processing status

--- pipeline_status.sh
+++ pipeline_status.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
-REGEX_PROC_PIPELINE='make\s+-j\s+[0-9]{1,3}\s+all\s+(run|experiment_id)=[^\s]+\s+(run|experiment_id)=.*'
+REGEX_PROC_PIPELINE='make\s+-j\s+[0-9]{1,3}\s+all\s+(run|experiment_id)=\w+\s+(run|experiment_id)=.*'
 
-if [[ $( pgrep -c -f "make\s+-j\s+[0-9]{1,3}\s+all\s+(run|experiment_id)=[^\s]+\s+(run|experiment_id)=.*" ) -gt 0 ]];
+if [[ $( pgrep -c -f "$REGEX_PROC_PIPELINE" ) -gt 0 ]];
 then
 	echo "List of currently running pipelines:"
 	ps -fp $( pgrep -f "$REGEX_PROC_PIPELINE" )
